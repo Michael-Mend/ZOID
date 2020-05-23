a636875dc380026c334057b338f36b65bfaf7fc0
Michael Mendoza <michael.mend97@yahoo.com>
2020-05-23 10:27:12 -0400

    !

---
+++ client/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'materialize-css/dist/css/materialize.min.css';
+import '../style/App.css'
+
+
+function Nav() {
+    return (
+        <nav className="nav">
+        </nav>
+    )
+};
+ export default Nav;
