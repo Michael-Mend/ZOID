cd0a71810e7c2f6d23aea9a3d1d8172046890d6d
Michael Mendoza <michael.mend97@yahoo.com>
2020-05-26 21:24:05 -0400

    finished basic ui

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Nav from "./Nav";
+import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+function Login() {
+    return (
+        <div>
+            <SearchBar />
+            <Nav />
+            <div className="signUpForm">
+                <p className="usernameLabel">username</p>
+                <input type="text"></input>
+                <p>password</p>
+                <input className="passwordInput" type="text"></input>
+                <Link to='/'>sign up</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
