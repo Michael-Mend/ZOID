c998890e034892026612ac9b34d0a5f83711b7cf
Michael-mend <michael.mend97@yahoo.com>
2020-06-09 10:17:37 -0400

    !

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,42 @@
 import React, { Component } from "react";
 import '../App.css';
 import expand from '../images/new2.png'
-// import { Link } from "react-router-dom";
 
 class Card extends Component {
+    constructor(props) {
+        super(props) 
+
+        this.state = {
+           class: 'hidden'
+        }
+    }
+
+    expand = () => {
+       this.setState({class: 'expanded'})
+    }
+
+    close = () => {
+        this.setState({class: 'hidden'})
+    }
+
     render() {
         return (
             <div className='cardMain'>
                 <div className='expand'>
-                    <button><img  src={expand} alt=''/></button>
+                    <button onClick={this.expand}><img  src={expand} alt=''/></button>
+                    <div className={this.state.class}>
+                        <button onClick={this.close} className='close'>close</button>
+                        <h3 className='title0'>{this.props.card.title}</h3>
+                        <p>{this.props.card.description}</p>
+                        <img alt='' src={this.props.card.image_link}/>
+                    </div>
                 </div>
                 <div className='card'>
                     <img title='reveal post' className='thumb' alt='' src={this.props.card.image_link}/>
                     <div> 
                         <div className='titleDiv'>
+                            <h4 className='usrnm'> {this.props.card.username} </h4>
                             <h3 className='title'>{this.props.card.title}</h3>
-                            <h4> {this.props.card.username} </h4>
                         </div>
                         <div className='postLinks'> 
                             <a className='postLink' href='/'>comment</a>
@@ -26,9 +47,6 @@ class Card extends Component {
                             <a className='postLink0' href='/'>report</a>
                         </div>
                     </div>
-                </div>
-                <div className='postBody'>
-                    <p>{this.props.card.description}</p>
                 </div>
             </div>
         )
