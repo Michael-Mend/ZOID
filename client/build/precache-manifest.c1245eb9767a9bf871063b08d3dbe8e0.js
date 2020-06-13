3fc8f701ff90ed4c66758111d697a854ad72f4bc
Antknee <anthonycolindres@yahoo.com>
2020-06-13 10:39:03 -0400

    ran npm build

---
+++ client/build/precache-manifest.c1245eb9767a9bf871063b08d3dbe8e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41ea35b1831427049962d41800a226e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eb5b16827211f7da881",
+    "url": "/static/css/main.d032e402.chunk.css"
+  },
+  {
+    "revision": "95b03754d15bc8bc5482",
+    "url": "/static/js/2.d47da5a2.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.d47da5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb5b16827211f7da881",
+    "url": "/static/js/main.3eff737b.chunk.js"
+  },
+  {
+    "revision": "2aae0a36387049204996",
+    "url": "/static/js/runtime-main.0a5377fc.js"
+  },
+  {
+    "revision": "d5803392c969752955ffcf7b253d0fea",
+    "url": "/static/media/new.d5803392.png"
+  },
+  {
+    "revision": "bea13b38f31d39a8bc1dee4d3f0d901f",
+    "url": "/static/media/new2.bea13b38.png"
+  }
+]);
